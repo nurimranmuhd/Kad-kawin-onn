e58999f901d0d4ec705609e7bdbd1c5c4f2a5cb5
nurimranmuhd <nurimranhalim@gmail.com>
2025-02-14 17:01:07 +0800

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,16 +1,15 @@
-document.getElementById("rsvp-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    let formData = new FormData(this);
-    let data = new URLSearchParams(formData);
-    
-    fetch("https://script.google.com/macros/s/AKfycbwwCpo8HX7FxV0ITXK_uJRBnQ5SgoBGpXSpNnOMAaXDVxc-wzi9PiBvCvkDyvspTqVw6A/dev", {  
+document.getElementById("ucapanForm")?.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var nama = document.getElementById("nama").value;
+    var pesanan = document.getElementById("pesanan").value;
+
+    fetch("https://script.google.com/macros/s/AKfycbzCfMP8FAyH7JQmbBBhJ77I2iZvEBwVdzKVl5RKK361/dev", {
         method: "POST",
-        body: data
-    })
-    .then(response => response.text())
+        body: JSON.stringify({ nama: nama, pesanan: pesanan }),
+        headers: { "Content-Type": "application/json" }
+    }).then(response => response.text())
     .then(data => {
-        alert("Terima kasih! RSVP anda telah direkod.");
-    })
-    .catch(error => console.error("Error:", error));
+        alert("Ucapan telah dihantar!");
+        document.getElementById("ucapanForm").reset();
+    }).catch(error => console.error("Error:", error));
 });
